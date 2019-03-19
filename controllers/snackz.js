8428db34bf123487fcd629f53eb42bc78a78526c
Cori Caridine <caridine.corin@gmail.com>
2019-03-19 16:47:39 -0400

    corrected image error

--- controllers/snackz.js
+++ controllers/snackz.js
@@ -14,49 +14,49 @@ const Snack = require('../models/snackz.js')
 //       snack: 'pastries',
 //       continent: 'South America',
 //       description: 'dessert',
-//       img: 'public/images/cooking-cuisine-delicious-908180.jpg'
+//       img: '../images/cooking-cuisine-delicious-908180.jpg'
 //     }, {
 //       snacker: 'Ian',
 //       snack: 'sushi',
 //       continent: 'Asia',
 //       description: 'fresh sushi',
-//       img: 'public/images/asia-carrot-chopsticks-357756.jpg'
+//       img: '../images/asia-carrot-chopsticks-357756.jpg'
 //     }, {
 //       snacker: 'Shawn',
 //       snack: 'avocado and stuff',
 //       continent: 'North America',
 //       description: 'healthy stuff',
-//       img: 'public/images/avocado-basil-close-up-1143754.jpg'
+//       img: '../images/avocado-basil-close-up-1143754.jpg'
 //     }, {
 //       snacker: 'Kylie',
 //       snack: 'chocolate cake',
 //       continent: 'Australia',
 //       description: 'yummy cake',
-//       img: 'public/images/adult-brawny-chocolate-cake-936000.jpg'
+//       img: '../images/adult-brawny-chocolate-cake-936000.jpg'
 //     }, {
 //       snacker: 'Gail',
 //       snack: 'bananas',
 //       continent: 'South America',
 //       description: 'crunchy sweetness',
-//       img: 'public/images/banana-bowl-chips-77029.jpg'
+//       img: '../images/banana-bowl-chips-77029.jpg'
 //     }, {
 //       snacker: 'Rob',
 //       snack: 'biscuits and a beverage',
 //       continent: 'Europe',
 //       description: 'afternoon snack',
-//       img: 'public/images/beverages-biscuits-book-213074.jpg'
+//       img: '../images/beverages-biscuits-book-213074.jpg'
 //     }, {
 //       snacker: 'Adrienne',
 //       snack: 'i do not know',
 //       continent: 'Antarctica',
 //       description: 'after the jetlag I cannot remember',
-//       img: 'public/images/appetizers-catering-cheese-6589.jpg'
+//       img: '../images/appetizers-catering-cheese-6589.jpg'
 //     }, {
 //       snacker: 'Tiffany',
 //       snack: 'cheese and stuff',
 //       continent: 'Europe',
 //       description: 'healthy yumminess',
-//       img: 'public/images/cheese-cuisine-diet-1239312.jpg'
+//       img: '../images/cheese-cuisine-diet-1239312.jpg'
 //     }
 //   ],
 //   (err, data) => {
